013528a51e0fab1c908385d8643921dc27bce3a7
SwathiRavi28 <68095540+SwathiRavi28@users.noreply.github.com>
2020-12-28 20:30:45 +0530

    second commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+dbPassword="mongodb://Swathi:1u0MiNtS5NWXrUa4@cluster0-shard-00-00.ann9b.mongodb.net:27017,cluster0-shard-00-01.ann9b.mongodb.net:27017,cluster0-shard-00-02.ann9b.mongodb.net:27017/Sample?ssl=true&replicaSet=atlas-11t65i-shard-0&authSource=admin&retryWrites=true&w=majority"
+module.exports = {
+    mongoURI: dbPassword
+};
